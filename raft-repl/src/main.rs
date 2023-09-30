f210d43d22e63663f5d3a342f8c905e0c340f814
Yo Eight <yo.eight@gmail.com>
2023-09-29 20:39:27 -0400

    Start setting up tui and crossterm.

--- raft-repl/src/main.rs
+++ raft-repl/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::execute;
+use crossterm::terminal::{
+    disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen, SetTitle,
+};
+use std::io;
+use tui::backend::CrosstermBackend;
+use tui::Terminal;
+
+fn main() -> eyre::Result<()> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, SetTitle("raft-playground"))?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
