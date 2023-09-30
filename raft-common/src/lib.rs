79fa6e06ec22bf6afab568b2fdb328edfbc60655
Yo Eight <yo.eight@gmail.com>
2023-09-29 20:11:17 -0400

    Export friendier types from the raft-common package.

--- raft-common/src/lib.rs
+++ raft-common/src/lib.rs
@@ -1,3 +1,13 @@
 mod proto {
     tonic::include_proto!("consensus");
 }
+
+pub mod client {
+    pub use super::proto::raft_client::*;
+}
+
+pub mod server {
+    pub use super::proto::raft_server::*;
+}
+
+pub use proto::{EntriesReq, EntriesResp, VoteReq, VoteResp};
