5825b15d2c7ac1cecc1a5aa9fdb45c2909733ca9
Yo Eight <yo.eight@gmail.com>
2023-10-03 14:50:35 -0400

    Add nodes table.

--- raft-repl/src/state.rs
+++ raft-repl/src/state.rs
@@ -2,15 +2,24 @@ use ratatui::backend::CrosstermBackend;
 use ratatui::layout::{Constraint, Layout};
 use ratatui::prelude::{Alignment, Direction};
 use ratatui::style::{Modifier, Style};
-use ratatui::widgets::{Block, Borders, List, ListItem, ListState};
+use ratatui::widgets::{Block, Borders, Cell, List, ListItem, ListState, Row, Table, TableState};
 use ratatui::Frame;
 use ratatui_textarea::{Input, TextArea};
 use std::io::StdoutLock;
+use uuid::Uuid;
+
+pub struct Node {
+    id: Uuid,
+    term: u64,
+    state: u64,
+}
 
 pub struct State {
     view: View,
     events: Vec<ListItem<'static>>,
     events_state: ListState,
+    nodes: Vec<Node>,
+    nodes_state: TableState,
 }
 
 impl State {
@@ -19,12 +28,33 @@ impl State {
             view: View::new(),
             events: vec![],
             events_state: ListState::default(),
+            nodes: vec![],
+            nodes_state: TableState::default(),
         }
     }
 
     pub fn draw(&mut self, frame: &mut Frame<CrosstermBackend<StdoutLock>>) {
         let main_chunks = self.view.main_layout.split(frame.size());
         let content_chunks = self.view.content_layout.split(main_chunks[0]);
+
+        let node_table = Table::new(vec![])
+            .header(self.view.node_table_header.clone())
+            .highlight_style(Style::default().add_modifier(Modifier::REVERSED))
+            // .style(Style::default().add_modifier(Modifier::UNDERLINED))
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+                    .title_alignment(Alignment::Right)
+                    .title("Nodes"),
+            )
+            .widths(&[
+                Constraint::Percentage(33),
+                Constraint::Percentage(33),
+                Constraint::Percentage(33),
+            ]);
+
+        frame.render_stateful_widget(node_table, content_chunks[0], &mut self.nodes_state);
+
         let event_list = List::new(self.events.clone()).block(
             Block::new()
                 .title("Events")
@@ -61,6 +91,7 @@ struct View {
     shell: TextArea<'static>,
     main_layout: Layout,
     content_layout: Layout,
+    node_table_header: Row<'static>,
 }
 
 impl View {
@@ -85,6 +116,13 @@ impl View {
             shell,
             main_layout,
             content_layout,
+            node_table_header: Row::new([
+                Cell::from("Id").style(Style::default().add_modifier(Modifier::BOLD)),
+                Cell::from("Term").style(Style::default().add_modifier(Modifier::BOLD)),
+                Cell::from("State").style(Style::default().add_modifier(Modifier::BOLD)),
+            ])
+            .height(1)
+            .style(Style::default().add_modifier(Modifier::REVERSED)),
         }
     }
 }
